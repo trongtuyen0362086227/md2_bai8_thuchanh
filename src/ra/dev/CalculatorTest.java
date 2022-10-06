0186019507280b005ee8115f8115e836fa993743
unknown <you@example.com>
2022-10-06 08:09:00 -0700

    d

---
+++ src/ra/dev/CalculatorTest.java
@@ -0,0 +1,19 @@
+package ra.dev;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("Testting")
+    void testCalculateAdd() {
+        int a = 1;
+        int b = 1;
+        char o = '+';
+        int expected = 2;
+
+        int result = Calculator.calculate(a, b, o);
+        assertEquals(expected, result);
+    }
+
+}
